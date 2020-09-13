798a8d01c5338b66988cb9dac2533e438e2f6b65
ATWp <69421094+ATWp@users.noreply.github.com>
2020-09-13 09:19:07 +0300

    intresting print

--- FirstProgramm.go
+++ FirstProgramm.go
@@ -4,6 +4,12 @@ import "fmt"
 
 //компиляция:  go run FirstProgramm.go
 //первый build: go build FirstProgramm.go
+/*
+  Многострочный комментарий.
+*/
 func main() {
+	/*
+		Функция выводит HELLO в консоль
+	*/
 	fmt.Println("HELLO")
 }
